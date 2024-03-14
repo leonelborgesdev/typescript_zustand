13ad47dba3859f7ab0a63948e28913cf28cbc9ee
leonelborgesdev <leonel03t@gmail.com>
2024-03-13 23:50:21 -0400

    getPosts

--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -1,15 +1,27 @@
 import create from "zustand";
 
+interface Post{
+    id: number
+    title: string
+    body: string
+}
 interface CouterState{
     count: number
     title: string
     increment: (value:number) =>void
+    posts: Post[]
 }
 
 export const useCounterStore=create<CouterState>((set)=>({
     count: 10,
     title: "Some title",
+    posts: [],
     increment: (value: number)=> set(state=> ({
         count: state.count + value
-    }))
+    })),
+    getPosts: async()=>{
+        const res=await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts=await res.json()
+        console.log(posts)
+    }
 }))
